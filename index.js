2d3214000c4175b7a84b798dc4eca5ea71e6a007
Jubair <jubair.ahmed7946@gmail.com>
2023-11-06 18:38:54 +0600

    Add: job data get with email query

--- index.js
+++ index.js
@@ -41,41 +41,40 @@ const jobsCollection = client.db("workTalesDB").collection("jobs");
 const bidsCollection = client.db("workTalesDB").collection("bids");
 // Database collection ENDS
 
-// POST a job 
-app.post("/api/v1/jobs", async(req,res)=>{
+// POST a job
+app.post("/api/v1/jobs", async (req, res) => {
   const job = req.body;
-  const result = await jobsCollection.insertOne(job)
-  res.send(result)
-})
+  const result = await jobsCollection.insertOne(job);
+  res.send(result);
+});
 
 // GET jobs with category query
 app.get("/api/v1/jobs", async (req, res) => {
-    let query = {}
-    if(req.query?.category){
-
-        query = { category: req.query?.category };
-    }
+  let query = {};
+  if (req.query?.category) {
+    query = { category: req.query?.category };
+  } else if(req.query?.employer_email){
+    query = {employer_email: req.query?.employer_email}
+  }
   const result = await jobsCollection.find(query).toArray();
   res.send(result);
 });
 
+
 // GET a single job with id
-app.get("/api/v1/jobs/:id", async(req,res)=>{
+app.get("/api/v1/jobs/:id", async (req, res) => {
   const id = req.params?.id;
-  const query = {_id: new ObjectId(id)}
-  const result = await jobsCollection.findOne(query)
-  res.send(result)
-
-})
-
+  const query = { _id: new ObjectId(id) };
+  const result = await jobsCollection.findOne(query);
+  res.send(result);
+});
 
 // POST a bid
-app.post("/api/v1/bids", async(req,res)=>{
+app.post("/api/v1/bids", async (req, res) => {
   const bid = req.body;
-  const result = await bidsCollection.insertOne(bid)
-  res.send(result)
-})
-
+  const result = await bidsCollection.insertOne(bid);
+  res.send(result);
+});
 
 app.get("/", (req, res) => {
   res.send("worktales server is Running");
